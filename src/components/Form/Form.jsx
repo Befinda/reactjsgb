a373feb13a48f0e4581169802405fa2ff3cb82a3
Margarita Mareeva <beffinda@gmail.com>
2022-09-19 18:38:45 +0300

    установлен eslint

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,33 +1,32 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import style from "./Form.module.css"
+import style from './Form.module.css';
 
-export const Form = ({addMessage}) =>{
+export const Form = ({ addMessage }) => {
+  const [value, setValue] = useState('');
 
-const [value, setValue] = useState(''); 
-
-const handleSubmit=(ev)=>{
+  const handleSubmit = (ev) => {
     ev.preventDefault();
     addMessage({
-        author: 'USER',
-        value,
+      author: 'USER',
+      value,
     });
     setValue('');
-}
+  };
 
-    return <form onSubmit={handleSubmit} className ={style.formmes}>
-
-        <div className={style.wrap}>
-        <input 
-            className = {style.formmesInput} 
-            type="text" 
-            value={value}
-            onChange={(e)=>setValue(e.target.value)}/>
-        <button 
-            className = {style.formmesBtn}
-            type="submit"
-            disabled={!value}
-        >Отправить</button>
-        </div>
+  return (
+    <form onSubmit={handleSubmit} className={style.formmes}>
+      <div className={style.wrap}>
+        <input
+          className={style.formmesInput}
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className={style.formmesBtn} type="submit" disabled={!value}>
+          Отправить
+        </button>
+      </div>
     </form>
-}
+  );
+};
